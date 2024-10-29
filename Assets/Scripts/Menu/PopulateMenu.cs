9d7e7558c7d3cb5987adde12a8aadf65f84751b0
Luca <luca@teapot.ovh>
2024-10-29 19:34:15 +0100

    load menu data from json

--- Assets/Scripts/Menu/PopulateMenu.cs
+++ Assets/Scripts/Menu/PopulateMenu.cs
@@ -2,23 +2,23 @@ using System;
 using System.Collections.Generic;
 using MixedReality.Toolkit.UX;
 using TMPro;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
-class Item {
-    public String name;
 
-    public Item(String name) => this.name = name;
+[Serializable]
+class Tasks {
+    public List<Entry> tasks;
 }
 
-class Section {
-    public String title;
-    public List<Item> items;
-
-    public Section(String tl, List<Item> itms) {
-        title = tl;
-        items = itms;
-    }
+[Serializable]
+class Entry {
+    public int id;
+    public int priority;
+    public string train_number;
+    public string door_number;
+    public string problem;
 }
 
 public class PopulateMenu : MonoBehaviour
@@ -29,42 +29,43 @@ public class PopulateMenu : MonoBehaviour
 
     public string sceneName;
 
-    private List<Section> sections = new List<Section>() {
-        new("Section 1", new List<Item>() {
-            new("Task 1"),
-            new("Task 2")
-        }),
-    };
+    public TextAsset jsonFile;
 
     // Start is called before the first frame update
     void Start()
     {
+        Console.WriteLine(jsonFile.ToString());
+        Tasks tasks = JsonUtility.FromJson<Tasks>(jsonFile.ToString());
+        foreach (var entry in tasks.tasks)
+        {
+            Console.WriteLine("aooo", entry);
+        }
+
         Debug.Log("root of the UI we're building is", gameObject);
         // We assume that the script is attached to a UIContainer, which always
         // has a ManipulatorContainer as a child
         Transform manipulationContainer = transform.GetChild(0);
         Assert.AreNotEqual(manipulationContainer, null);
-        foreach (Section section in sections) {
-            GameObject sec = Instantiate(listContainer, new Vector3(0, 0, 0), Quaternion.identity, manipulationContainer);
+
+        GameObject sec = Instantiate(listContainer, new Vector3(0, 0, 0), Quaternion.identity, manipulationContainer);
+        // Force local transform to be relative to the parent
+        sec.transform.localPosition = new Vector3(0, 0, 0);
+        // We assume that a listContainer GameObject always has a first child that is the title
+        TextMeshProUGUI titleText = sec.GetComponentInChildren<TextMeshProUGUI>();
+        Assert.AreNotEqual(titleText, null);
+        titleText.SetText("Tasks");
+
+        GameObject grid = Instantiate(listGrid, new Vector3(0, 0, 0), Quaternion.identity, sec.transform);
+        grid.transform.localPosition = new Vector3(0, 0, 0);
+        foreach (Entry entry in tasks.tasks) {
+            GameObject itm = Instantiate(listEntry, new Vector3(0, 0, 0), Quaternion.identity, grid.transform);
+            addItemAction(itm);
+
             // Force local transform to be relative to the parent
-            sec.transform.localPosition = new Vector3(0, 0, 0);
-            // We assume that a listContainer GameObject always has a first child that is the title
-            TextMeshProUGUI titleText = sec.GetComponentInChildren<TextMeshProUGUI>();
-            Assert.AreNotEqual(titleText, null);
-            titleText.SetText(section.title);
-
-            GameObject grid = Instantiate(listGrid, new Vector3(0, 0, 0), Quaternion.identity, sec.transform);
-            grid.transform.localPosition = new Vector3(0, 0, 0);
-            foreach (Item item in section.items) {
-                GameObject itm = Instantiate(listEntry, new Vector3(0, 0, 0), Quaternion.identity, grid.transform);
-                addItemAction(itm);
-
-                // Force local transform to be relative to the parent
-                itm.transform.localPosition = new Vector3(0, 0, 0);
-                TextMeshProUGUI itemText = itm.GetComponentInChildren<TextMeshProUGUI>();
-                Assert.AreNotEqual(itemText, null);
-                itemText.SetText(item.name);
-            }
+            itm.transform.localPosition = new Vector3(0, 0, 0);
+            TextMeshProUGUI itemText = itm.GetComponentInChildren<TextMeshProUGUI>();
+            Assert.AreNotEqual(itemText, null);
+            itemText.SetText(entry.problem);
         }
     }
 
